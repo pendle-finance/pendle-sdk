49716e7a0cb22a15c7c3b941a70ffc4d1730b92d
joesawtw1 <joesawtw@gmail.com>
2021-09-10 11:26:08 +0800

    implemented get redeem, swap and liquidity transactions

--- src/entities/transactions/transactionClass.ts
+++ src/entities/transactions/transactionClass.ts
@@ -33,7 +33,7 @@ export class Transaction {
     queryObj: SubgraphQuery
   ): Promise<TRANSACTION[]> {
     // TODO
-    // , where: {forgeId: "${forgeId}", expiry: ${expiry}}
+    // , where: {forgeId: "${forgeId}", expiry: ${expiry}} underlying yield
     const query = gql`{
             mintYieldTokens(first: ${queryObj.limit}, orderBy: timestamp, orderDirection: desc) {
                 id
@@ -86,6 +86,171 @@ export class Transaction {
           new Token(mintObj.otAsset.id, mintObj.otAsset.decimals),
           mintObj.amountMinted
         ),
+      ],
+      network: 'mainnet',
+    }));
+  }
+
+  public async getRedeemTransactions(queryObj: SubgraphQuery) {
+    const query = gql`{
+      redeemYieldTokens(first: ${queryObj.limit}, orderBy: timestamp, orderDirection: desc) {
+          id
+          timestamp
+          redeemedValueUSD
+          amountToRedeem
+          amountRedeemed
+          expiry
+          xytAsset {
+              id
+              decimals
+          }
+          otAsset {
+              id
+              decimals
+          }
+          yieldBearingAsset {
+              id
+              symbol
+              decimals
+          }
+          from
+      }
+  }`;
+
+    const response = await request(
+      PendleSubgraphUrlMapping[this.network],
+      query
+    );
+
+    return response.redeemYieldTokens.map((redeemObj: any) => ({
+      action: 'Redeem',
+      hash: redeemObj.id,
+      amount: { currency: 'USD', amount: redeemObj.redeemedValueUSD },
+      paid: [
+        new TokenAmount(
+          new Token(redeemObj.xytAsset.id, redeemObj.xytAsset.decimals),
+          redeemObj.amountRedeemed
+        ),
+        new TokenAmount(
+          new Token(redeemObj.otAsset.id, redeemObj.otAsset.decimals),
+          redeemObj.amountRedeemed
+        ),
+      ],
+      received: [
+        new TokenAmount(
+          new Token(
+            redeemObj.yieldBearingAsset.id,
+            redeemObj.yieldBearingAsset.decimals
+          ),
+          redeemObj.amountToRedeem
+        ),
+      ],
+      network: 'mainnet',
+    }));
+  }
+
+  public async getSwapTransactions(queryObj: SubgraphQuery) {
+    const query = gql`{
+      swaps(first: ${queryObj.limit}, orderBy: timestamp, orderDirection: desc) {
+          id
+          timestamp
+          amountUSD
+          inAmount
+          outAmount
+          inToken {
+              id
+              decimals
+              symbol
+          }
+          outToken {
+              id
+              decimals
+              symbol
+          }
+          from
+      }
+  }`;
+
+    const response = await request(
+      PendleSubgraphUrlMapping[this.network],
+      query
+    );
+
+    return response.swaps.map((swapObj: any) => ({
+      action: 'Swap',
+      hash: swapObj.id,
+      amount: { currency: 'USD', amount: swapObj.amountUSD },
+      paid: [
+        new TokenAmount(
+          new Token(swapObj.inToken.id, swapObj.inToken.decimals),
+          swapObj.inAmount
+        ),
+      ],
+      received: [
+        new TokenAmount(
+          new Token(swapObj.outToken.id, swapObj.outToken.decimals),
+          swapObj.outAmount
+        ),
+      ],
+      network: 'mainnet',
+    }));
+  }
+
+  public async getLiquidityTransactions(
+    queryObj: SubgraphQuery
+  ): Promise<TRANSACTION[]> {
+    // TODO
+    // , where: {forgeId: "${forgeId}", expiry: ${expiry}} underlying yield
+    const query = gql`{
+            liquidityPools(first: ${queryObj.limit}, orderBy: timestamp, orderDirection: desc) {
+                id
+                timestamp
+                inAmount0
+                inAmount1
+                inToken0 {
+                  id
+                  symbol
+                  decimals
+                }
+                inToken1 {
+                  id
+                  symbol
+                  decimals
+                }
+                type
+                amountUSD
+                from
+            }
+        }`;
+
+    const response = await request(
+      PendleSubgraphUrlMapping[this.network],
+      query
+    );
+
+    return response.liquidityPools.map((liquidityObj: any) => ({
+      action: liquidityObj.type,
+      hash: liquidityObj.id,
+      amount: { currency: 'USD', amount: liquidityObj.amountUSD },
+      paid: [
+        new TokenAmount(
+          new Token(liquidityObj.inToken0.id, liquidityObj.inToken0.decimals),
+          liquidityObj.inAmount0
+        ),
+        new TokenAmount(
+          new Token(liquidityObj.inToken1.id, liquidityObj.inToken1.decimals),
+          liquidityObj.inAmount1
+        ),
+      ],
+      received: [
+        // new TokenAmount(
+        //   new Token(mintObj.xytAsset.id, mintObj.xytAsset.decimals),
+        //   mintObj.amountMinted
+        // ),
+        // new TokenAmount(
+        //   new Token(mintObj.otAsset.id, mintObj.otAsset.decimals),
+        //   mintObj.amountMinted
+        // ),
       ],
       network: 'mainnet',
     }));
--- test/subgraphTransactions.test.ts
+++ test/subgraphTransactions.test.ts
@@ -13,6 +13,21 @@ describe('Transaction Entity', () => {
 
     it('Transaction.getMintTransactions', async () => {
         const result = await transaction.getMintTransactions({page:1, limit:1})
-        console.log('result', result)
+        console.log('getMintTransactions', result)
+    })
+
+    it('Transaction.getRedeemTransactions', async () => {
+        const result = await transaction.getRedeemTransactions({page:1, limit:1})
+        console.log('getRedeemTransactions', result)
+    })
+
+    it('Transaction.getSwapTransactions', async () => {
+        const result = await transaction.getSwapTransactions({page:1, limit:1})
+        console.log('getSwapTransactions', result)
+    })
+
+    it('Transaction.getLiquidityTransactions', async () => {
+        const result = await transaction.getLiquidityTransactions({page:1, limit:1})
+        console.log('getLiquidityTransactions', result)
     })
 })
