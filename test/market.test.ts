cca8417f3d9c47fd86daf5badf298fd773d91f2a
Hanabi-R <hana.rei.andata@gmail.com>
2021-09-18 21:42:17 +0800

    update version

--- test/market.test.ts
+++ test/market.test.ts
@@ -32,22 +32,24 @@ const tokens = { USDCToken, aUSDCYTToken, PENDLEToken, PENDLEETHYTToken, ETHUSDC
 const PendleEthMarket = PendleMarket.find("0x685d32f394a5F03e78a1A0F6A91B4E2bf6F52cfE", 1);
 const ETHUSDCMarket = PendleMarket.find("0x79c05Da47dC20ff9376B2f7DbF8ae0c994C3A0D0", 1);
 const aUSDC2022Market = PendleMarket.find("0x8315bcbc2c5c1ef09b71731ab3827b0808a2d6bd",1);
+
+const aUSDC2021Market = PendleMarket.find("0x9e382e5f78b06631e4109b5d48151f2b3f326df0",1);
 const cDAI2022Market = PendleMarket.find("0xb26c86330fc7f97533051f2f8cd0a90c2e82b5ee", 1);
 
 
-const markets = { PendleEthMarket, ETHUSDCMarket, aUSDC2022Market, cDAI2022Market };
+const markets = { PendleEthMarket, ETHUSDCMarket, aUSDC2022Market, cDAI2022Market, aUSDC2021Market };
 describe("Market", () => {
     let provider: ethers.providers.JsonRpcProvider;
     let signer: any;
     let market: PendleMarket;
 
     beforeAll(async () => {
-        // const providerUrl = `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`;
+        const providerUrl = `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`;
 
-        const providerUrl = `http://127.0.0.1:8545`;
+        // const providerUrl = `http://127.0.0.1:8545`;
         provider = new ethers.providers.JsonRpcProvider(providerUrl);
         signer = provider.getSigner("0xb69da28b6b5ddf0fd4fee4823a3ffd2243a13c92");
-        market = markets.cDAI2022Market;
+        market = markets.aUSDC2021Market;
     });
 
     it("PendleMarket.readMarketDetails", async () => {
@@ -69,21 +71,21 @@ describe("Market", () => {
         console.log(swapExactInDetails);
     })
 
-    it('PendleMarket.swapExactOutDetails', async () => {
+    it.only('PendleMarket.swapExactOutDetails', async () => {
         const swapExactOutDetails = await market.methods(signer).swapExactOutDetails(new TokenAmount(
             market.tokens[1],
-            BN.from(10).pow(12).toString()
+            BN.from(10).pow(16).toString()
         ),
         0.01);
         console.log(swapExactOutDetails);
     })
 
-    it.skip('PendleMarket.swapExactIn', async () => {
+    it('PendleMarket.swapExactIn', async () => {
         const response = await market.methods(signer).swapExactIn(0.01, dummyTokenAmount);
         console.log(response);
     })
 
-    it.skip('PendleMarket.swapExactOut', async () => {
+    it('PendleMarket.swapExactOut', async () => {
         const response = await market.methods(signer).swapExactOut(0.01, dummyTokenAmount);
         console.log(response);
     });
@@ -96,7 +98,7 @@ describe("Market", () => {
         console.log(response);
     })
 
-    it.skip('PendleMarket.addDual', async () => {
+    it('PendleMarket.addDual', async () => {
         const response = await market.methods(signer).addDual([dummyTokenAmount, dummyTokenAmount], 0.001);
         console.log(response);
     })
@@ -109,7 +111,7 @@ describe("Market", () => {
         console.log(response);
     })
 
-    it.skip('PendleMarket.addSingle', async () => {
+    it('PendleMarket.addSingle', async () => {
         const response = await market.methods(signer).addSingle(dummyTokenAmount, 0.001);
         console.log(response);
     })
@@ -119,7 +121,7 @@ describe("Market", () => {
         console.log(response);
     })
 
-    it.skip('PendleMarket.removeDual', async () => {
+    it('PendleMarket.removeDual', async () => {
         const response = await market.methods(signer).removeDual(0.5, 0.001);
         console.log(response);
     })
@@ -129,7 +131,7 @@ describe("Market", () => {
         console.log(JSON.stringify(response, null, '  '));
     })
 
-    it.skip('PendleMarket.removeSingle', async () => {
+    it('PendleMarket.removeSingle', async () => {
         const response = await market.methods(signer).removeSingle(0.5, dummyToken, 0.001);
         console.log(response);
     })
