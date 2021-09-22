456f7f2e6437c5034adab2c1d32782c4cee1bb2f
Hanabi-R <hana.rei.andata@gmail.com>
2021-09-23 07:40:12 +0800

    revert tests

--- test/sdk.test.ts
+++ test/sdk.test.ts
@@ -1,45 +1,32 @@
-import { Yt, StakingPool, PendleMarket, dummyAddress } from '../src';
+import { Yt, StakingPool, PendleMarket, dummyAddress, contracts } from '../src';
 // import { Market } from '../src/entities/market';
-// import { assert, expect } from 'chai';
-const { waffle } = require('hardhat');
-
-const { provider } = waffle;
-import { evm_revert, evm_snapshot } from './helpers/Evm';
+import { ethers, Contract } from 'ethers';
+import * as dotenv from 'dotenv';
+dotenv.config();
+jest.setTimeout(30000);
 
 const dummyUser = '0x82c9D29739333258f08cD3957d2a7ac7f4d53fAb'; // Mainnet test account
 
 describe('Sdk', () => {
-  // let provider: ethers.providers.JsonRpcProvider;
+  let provider: ethers.providers.JsonRpcProvider;
   let signer: any;
-  let globalSnapshotId: string, snapshotId: string;
 
-  before(async () => {
-    // const providerUrl = `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`;
+  beforeAll(async () => {
+    const providerUrl = `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`;
 
     // const providerUrl = `http://127.0.0.1:8545`;
-    // provider = new ethers.providers.JsonRpcProvider(providerUrl);
+    provider = new ethers.providers.JsonRpcProvider(providerUrl);
     signer = provider.getSigner();
-    globalSnapshotId = await evm_snapshot();
-    snapshotId = await evm_snapshot();
   });
 
-  after(async () => {
-    await evm_revert(globalSnapshotId);
-  });
-
-  beforeEach(async () => {
-    await evm_revert(snapshotId);
-    snapshotId = await evm_snapshot();
-  });
-
-  it('PendleMarket.find', async () => {
-    const market = await PendleMarket.find(
+  it('fetchPendleMarketData', async () => {
+    const market = PendleMarket.find(
       '0x944d1727d0b656f497e74044ff589871c330334f'
     );
     console.log(JSON.stringify(market, null, '  '));
   });
 
-  it.only('Market.methods.fetchInterests', async () => {
+  it('Market.methods.fetchInterests', async () => {
     const userInterests = await PendleMarket.methods(signer).fetchInterests(
       dummyUser
     );
@@ -58,20 +45,23 @@ describe('Sdk', () => {
     console.log(JSON.stringify(xyt, null, '  '))
   })
 
-  it('StakingPool.methods.fetchInterestsAndRewards', async () => {
-    const interestsAndRewards = await StakingPool.methods(
-      signer
-    ).fetchClaimableYields(
-      dummyUser
-    );
-    console.log(JSON.stringify(interestsAndRewards, null, '  '));
+  it.only('StakingPool.methods.fetchInterestsAndRewards', async () => {
+    const lmV1Contract = new Contract("0x5b1c59eb6872f88a92469751a034b9b5ada9a73f", contracts.PendleLiquidityMiningBase.abi, provider);
+    const d = await lmV1Contract.readUserSpecificExpiryData(1672272000, "0x7e733777a04b9fadf5ecd8e2bad303cabbd65cd6", {blockTag: 12648940});
+    console.log(JSON.stringify(d, null, '  '));
+    // const interestsAndRewards = await StakingPool.methods(
+    //   signer
+    // ).fetchClaimableYields(
+    //   "0xa2188a2d67fb39e386def087f43b9407aabe5c0e"
+    // );
+    // console.log(JSON.stringify(interestsAndRewards, null, '  '));
   });
 
-  it.only('StakingPool.methods.fetchAccruingRewards', async () => {
+  it('StakingPool.methods.fetchAccruingRewards', async () => {
     const accruingRewards = await StakingPool.methods(
       signer
     ).fetchAccruingRewards(
-      dummyUser
+      dummyAddress
     );
     console.log(JSON.stringify(accruingRewards, null, '  '));
   });
