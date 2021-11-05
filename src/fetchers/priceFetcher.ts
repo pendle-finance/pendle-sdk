59691992b6991cb102af931eb6284f41d9433fd3
Hanabi-R <hana.rei.andata@gmail.com>
2021-11-05 09:29:04 +0000

    bug fix

--- src/fetchers/priceFetcher.ts
+++ src/fetchers/priceFetcher.ts
@@ -98,10 +98,10 @@ export async function fetchYtPrice(yt: Yt, signer: providers.JsonRpcSigner, chai
 
 export async function fetchCTokenPrice(address: string, signer: providers.JsonRpcSigner, chainId: number|undefined): Promise<BigNumber> {
   const networkInfo: NetworkInfo = distributeConstantsByNetwork(chainId);
-  const cTokenContract: Contract = new Contract(address, contracts.ICToken, signer.provider);
-  const underlyingAsset: string = await cTokenContract.underlying();
+  const cTokenContract: Contract = new Contract(address, contracts.ICToken.abi, signer.provider);
+  const underlyingAsset: string = await cTokenContract.callStatic.underlying();
   const exchangeRate: BN = await cTokenContract.callStatic.exchangeRateCurrent();
-  const adjustedExchangeRate: BigNumber = new BigNumber(exchangeRate.toString()).div(decimalFactor(10+networkInfo.decimalsRecord[underlyingAsset]));
+  const adjustedExchangeRate: BigNumber = new BigNumber(exchangeRate.toString()).div(decimalFactor(10 + networkInfo.decimalsRecord[underlyingAsset.toLowerCase()]));
   return adjustedExchangeRate.multipliedBy(await fetchTokenPrice({address: underlyingAsset, signer, chainId}));
 }
 
